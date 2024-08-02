5e6af6fa843442604086dfd37f2020b0a406f505
Marvin Ramos <marvinramos.nutnull@gmail.com>
2024-08-03 04:41:28 +0800

    added styles and script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,32 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite([
+
+            'public/css/jquery-jvectormap-1.2.2.css',
+            'public/css/dataTables.bootstrap4.min.css',
+            'public/css/responsive.bootstrap4.min.css',
+            'public/css/bootstrap.min.css',
+            'public/css/icons.min.css',
+            'public/css/admin-app.min.css',
+            'resources/css/app.css',
+            
+            'public/js/jquery.min.js',
+            'public/js/bootstrap.bundle.min.js',
+            'public/js/metisMenu.min.js',
+            'public/js/simplebar.min.js',
+            'public/js/waves.min.js',
+            'public/js/apexcharts.min.js',
+            'public/js/jquery-jvectormap-1.2.2.min.js',
+            'public/js/jquery-jvectormap-us-merc-en.js',
+            'public/js/jquery.dataTables.min.js',
+            'public/js/dataTables.bootstrap4.min.js',
+            'public/js/responsive.bootstrap4.min.js',
+            'public/pages/js/dashboard.init.js',
+            'public/js/admin-app.js',
+            'resources/js/app.js',
+            
+        ])
 
     </head>
 
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,27 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'public/css/jquery-jvectormap-1.2.2.css',
+                'public/css/dataTables.bootstrap4.min.css',
+                'public/css/responsive.bootstrap4.min.css',
+                'public/css/bootstrap.min.css',
+                'public/css/icons.min.css',
+                'public/css/admin-app.min.css',
                 'resources/css/app.css',
+                
+                'public/js/jquery.min.js',
+                'public/js/bootstrap.bundle.min.js',
+                'public/js/metisMenu.min.js',
+                'public/js/simplebar.min.js',
+                'public/js/waves.min.js',
+                'public/js/apexcharts.min.js',
+                'public/js/jquery-jvectormap-1.2.2.min.js',
+                'public/js/jquery-jvectormap-us-merc-en.js',
+                'public/js/jquery.dataTables.min.js',
+                'public/js/dataTables.bootstrap4.min.js',
+                'public/js/responsive.bootstrap4.min.js',
+                'public/pages/js/dashboard.init.js',
+                'public/js/admin-app.js',
                 'resources/js/app.js',
             ],
             refresh: true,
