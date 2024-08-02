facaabb99d01041644d318db8fe3adc2d275fc1f
Marvin Ramos <marvinramos.nutnull@gmail.com>
2024-08-03 07:12:04 +0800

    updated added logout functionality

--- resources/views/partials/admin/header.blade.php
+++ resources/views/partials/admin/header.blade.php
@@ -65,12 +65,26 @@
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
-                    <a class="dropdown-item" href="#"><i class="ri-user-line align-middle me-1"></i> Profile</a>
-                    <a class="dropdown-item" href="#"><i class="ri-wallet-2-line align-middle me-1"></i> My Wallet</a>
-                    <a class="dropdown-item d-block" href="#"><span class="badge bg-success float-end mt-1">11</span><i class="ri-settings-2-line align-middle me-1"></i> Settings</a>
-                    <a class="dropdown-item" href="#"><i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen</a>
+                    <a class="dropdown-item" href="#">
+                        <i class="ri-user-line align-middle me-1"></i> Profile
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <i class="ri-wallet-2-line align-middle me-1"></i> My Wallet
+                    </a>
+                    <a class="dropdown-item d-block" href="#">
+                        <span class="badge bg-success float-end mt-1">11</span>
+                        <i class="ri-settings-2-line align-middle me-1"></i> Settings
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <i class="ri-lock-unlock-line align-middle me-1"></i> Lock screen
+                    </a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item text-danger" href="#"><i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="dropdown-item text-danger">
+                        @csrf
+                        <button type="submit" class="btn btn-link text-danger p-0 m-0">
+                            <i class="ri-shut-down-line align-middle me-1 text-danger"></i> Logout
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// ADMIN FUNCTIONALITY
+Route::controller(MainController::class)->group(function () {
+    Route::get('/admin/logout')->name('admin.logout');
 });
 
 require __DIR__.'/auth.php';
